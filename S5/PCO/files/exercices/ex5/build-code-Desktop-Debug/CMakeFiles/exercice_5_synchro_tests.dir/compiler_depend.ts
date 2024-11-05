655345c84df44f652bb442cffe892fb42a6ce7e3
grinlemon <73121793+grinlemon@users.noreply.github.com>
2024-11-05 13:30:22 +0100

    chore: remove exercices folder

--- S5/PCO/files/exercices/ex5/build-code-Desktop-Debug/CMakeFiles/exercice_5_synchro_tests.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for exercice_5_synchro_tests.
--- S5/PCO/files/exercices/ex5/build-code-Desktop-Debug/CMakeFiles/exercice_5_synchro_tests.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for exercice_5_synchro_tests.
-# This may be replaced when dependencies are built.
