78803a0ccdabe303805127feb0d76f3dabb0cad9
Guillaume Trüeb <guillaumetrueb@etik.com>
2025-09-24 10:04:02 +0200

    exs: PLP C1

--- S7/PLP/files/exercices/C1/corr/unique.hs
+++ S7/PLP/files/exercices/C1/corr/unique.hs
@@ -13,3 +13,12 @@ unique' (x:xs) = if isInList x xs
   where
     isInList _ [] = False
     isInList y (z:zs) = (y == z) || isInList y zs
+
+
+-- the best approach
+unique'' xs = unique'' xs []
+    where
+        unique'' [] acc = reverse acc           -- check if list is empty then reverse the accumulator
+        unique'' (x:xs) acc
+            | x `elem` acc = unique'' xs acc
+            | otherwise    = unique'' xs (x:acc)
